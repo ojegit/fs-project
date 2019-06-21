50a5fc7410ca4ac1a52d367e5cc75d8d3944c0cb
clarkjrcanlog <clarkcanlog@gmail.com>
2019-06-20 19:54:57 -0700

    back up working authentication

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' +config.database);
+})
+
+//on error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error ' +err);
+})
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+// CORS middleware
+app.use(cors());
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body-parser middleware
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+})
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+})
